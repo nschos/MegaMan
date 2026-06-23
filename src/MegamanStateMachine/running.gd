d5635495e893132656fb23e9d513177b86374362
nschos <nathanschostack@hotmail.com>
2026-06-22 21:56:38 -0300

    moving script files (Megaman state machine scripts

--- src/MegamanStateMachine/running.gd
+++ src/MegamanStateMachine/running.gd
@@ -9,7 +9,7 @@ const frame_inch_d_window := 7
 enum Velocity { ACCELERATE, DECELERATE, FULLSPEED, DECELERATE_FROM_FULLSPEED, FULLHALT, NULL }
 
 var current_velocity: Velocity = Velocity.FULLHALT
-var last_direction := 0
+
 
 func enter(previous_state_path: String, data := {}) -> void:
 	print("running!")
@@ -26,33 +26,38 @@ func physics_update(_delta: float) -> void:
 	
 	var direction := Input.get_axis("ui_left", "ui_right")
 	
-	#print(direction)
 	
 	# ACCELERATE
 	if direction != 0:
-		last_direction = direction
+		
+		print("direction: ",direction)
+
 		if current_velocity == Velocity.FULLHALT or current_velocity == Velocity.DECELERATE or current_velocity == Velocity.DECELERATE_FROM_FULLSPEED:
 			current_velocity = Velocity.ACCELERATE
-			megaman.velocity.x = direction * megaman.RUNNING_ACCELERATION_X
 			frame_counter = 0
 			
 		elif current_velocity == Velocity.ACCELERATE:
 			frame_counter += 1
 			if frame_counter == frame_inch_window:
 				megaman.animation_player.play("inch")
-			elif frame_counter == frame_acceleration_window:
+			elif frame_counter >= frame_acceleration_window:
 				current_velocity = Velocity.FULLSPEED
-				megaman.velocity.x = direction * megaman.RUNNING_FULLSPEED_X
 				megaman.animation_player.play("walk")
-		
-		elif current_velocity == Velocity.FULLSPEED:
-			megaman.velocity.x = direction * megaman.RUNNING_FULLSPEED_X
+			
+		match current_velocity:
+			Velocity.FULLSPEED:
+				megaman.velocity.x = direction * megaman.RUNNING_FULLSPEED_X
+			Velocity.ACCELERATE:
+				megaman.velocity.x = direction * megaman.RUNNING_ACCELERATION_X
+				
+		print("current velocity: ",current_velocity)
+		print("megaman velocity x:",megaman.velocity.x)
 		
 	# DECELERATE
 	else:
 		if current_velocity == Velocity.FULLSPEED:
 			current_velocity = Velocity.DECELERATE_FROM_FULLSPEED
-			megaman.velocity.x = last_direction * megaman.RUNNING_DECELERATION_X
+			megaman.velocity.x = direction * megaman.RUNNING_DECELERATION_X
 			frame_counter = 0
 		
 		elif current_velocity == Velocity.DECELERATE_FROM_FULLSPEED:
